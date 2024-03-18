0e7181290226ea1bb3ab54bc2ec0c9f8e8de9711
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-03-18 16:46:09 +0530

    Created Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '../assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
@@ -8,8 +9,9 @@ export const metadata = {
 const MainLayout = ({children}) => {
   return (
     <html lang='en'>
-    <body>
-      <div>{children}</div>
+          <body>
+              <Navbar/>
+      <main>{children}</main>
       </body>
       </html>
   )
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,6 @@
+'use client';
+
+const PropertyPage = () => {
+    return <div>Property Page</div>;
+}
+export default PropertyPage;
